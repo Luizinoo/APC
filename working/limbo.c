c22ab97cffc8a78883d20742015b41d4c5809dc6
Luizino <luis.luizino@gmail.com>
2022-12-15 15:17:23 -0300

    savando

--- working/limbo.c
+++ working/limbo.c
@@ -7,8 +7,17 @@ int main(){
 
     for (int linhas = 1; linhas <= numero; linhas++){
         for (int colunas = 1; colunas <= linhas; colunas++){
-            printf("%d ", colunas);
-        } 
+            printf("%2d ", linhas);
+        }
+        printf("\n");
+    }
+
+    printf("\n");
+
+    for (int linhas = 1; linhas <= numero; linhas++){
+        for (int colunas = 1; colunas <= linhas; colunas++){
+            printf("%2d ", colunas);
+        }
         printf("\n");
     }
     
