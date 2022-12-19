a9686a94893cddc378b1cb67ee5a21ec74b34be5
Luizino <luis.luizino@gmail.com>
2022-12-19 18:02:31 -0300

    kkkk

--- working/limbo.c
+++ working/limbo.c
@@ -10,29 +10,21 @@ int main(){
     scanf("%d", &linhas);
 
     for (int i = 1; i <= linhas; i++){
-        system("pause");
-        system("cls");
         scanf("%d", &numero);
         for(int j = 2; j<numero; j++ ){
+            ehprimo=true;
             if (0==numero%j){
                 ehprimo = false;
-                // printf("nao e primo\n");
                 break;
             }
-            // else{
-            //     printf("e primo\n");
-            //     break;
-            // }
         }
-        if (ehprimo)
-        {
+        if (ehprimo){
             printf("e primo\n");
         }
         else{
             printf("nao e primo\n");
         }
-        
-        
     }
+
     return 0;
 }
