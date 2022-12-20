d4b5348d7252e31b1231ed435c246c83d49000c3
Luizino <luis.luizino@gmail.com>
2022-12-20 15:36:30 -0300

    atualizando

--- working/limbo.c
+++ working/limbo.c
@@ -2,37 +2,43 @@
 #include<stdbool.h>
 #include<math.h>
 
+bool checaprimo (numero){
+    
+    bool ehprimo;
+
+    for(int j = 2; j<numero; j++ ){
+        ehprimo=true;
+            if (0 == numero%j){
+                ehprimo = false;
+            break;
+        }
+    }return ehprimo;
+}
+
+bool checaarrojado (numero){
+
+    while(numero!=0){
+        if (checaprimo(numero)){
+            numero = numero/10;
+        }else{
+            return false;
+        }
+    }return true;
+}
+
 int main(){
     int linhas, numero;
-    bool ehprimo = true;
-
-    system("cls");
 
     scanf("%d", &linhas);
 
-    ehprimo=
-
-    while(numero!=0){
-        numero=numero/10;
-        printf("%d\n", numero);
-        break;
+    for(int i = 1; i <= linhas; i++){
+        scanf("%d", &numero);
+        if (checaarrojado(numero)){
+            printf("arrojado\n");
+        }else{
+            printf("nao arrojado\n");
+        }
     }
 
     return 0;
-}
-
-int primo (candidato){
-
-    int numero;
-
-    for(int i = 1; i <= linhas; i++){
-        scanf("%d", &numero);
-        for(int j = 2; j<numero; j++ ){
-            ehprimo=true;
-            if (0==numero%j){
-                ehprimo = false;
-                break;
-            }
-        }
-    }
 }
