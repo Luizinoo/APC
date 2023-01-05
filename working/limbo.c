3cbeb3f2bece1c7a095b191678433f7e792aee59
Luizino <luis.luizino@gmail.com>
2023-01-04 23:10:16 -0300

    salvando

--- working/limbo.c
+++ working/limbo.c
@@ -3,21 +3,26 @@
 
 int main(){
 
-    char palavra[1001], aux;
+    char usuario[1001], aux;
 
-    scanf("%s", &palavra);
+    scanf("%s", &usuario);
 
-    int i, fim, tam = strlen(palavra);
+    printf("%s", usuario);
+
+    int i, fim, tam = strlen(usuario);
 
     fim = tam - 1;
     for(i = 0; i < tam/2; i++){
-        aux = palavra[i];
-        palavra[i] = palavra[tam-i-1];
-        palavra[tam-i-1] = aux;
+        aux = usuario[i];
+        usuario[i] = usuario[tam-i-1];
+        usuario[tam-i-1] = aux;
         fim--;
     }
-
-    printf("%s", palavra);
+    
+    printf(".");
+    printf("%s", usuario);
+    printf("@unb.br");
+    
 
     return 0;
 }
